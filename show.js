e9c7d6eb81c2741c67f9c9244677d7e6f806f7c3
leon <leon.thomanek@stud.h-da.de>
2020-06-06 15:04:34 +0200

    create a begining

--- show.js
+++ show.js
@@ -1,4 +1,15 @@
+window.onload = function () {
+    console.log('Dokument geladen');
+    (function set_canvas(){
+        var canvas = document.getElementsByTagName('canvas')[0];
+        canvas.width  = 600;
+        canvas.height = 600; 
+    }());
+     
+}
 
-var test = new Linie(new Punkt(0,0), new Punkt(1,1));
 
-test.say_line();
+//
+//ar test = new Linie(new Punkt(0,0), new Punkt(1,1));
+//
+//est.say_line();
