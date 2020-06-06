89cd2030e6a70b2a9c3df1b729c781b8499592a9
leon <leon.thomanek@stud.h-da.de>
2020-06-06 21:36:20 +0200

    fix array

--- show.js
+++ show.js
@@ -1,6 +1,4 @@
-
-var board = [30][30];
-
+var board;
 window.onload = function () {
     console.log('Dokument geladen');
     (function set_canvas(){
@@ -12,32 +10,49 @@ window.onload = function () {
     initBoard();
       
 }
+function get2DArray(cols,rown) {
+   var arr = new Array(cols);
+   for(var i =0; i < arr.length;i++){
+      arr[i] = new Array(rown);
+   }
+   return arr;
+}
+
+function default_init_array(arry, defalut_value) {
+
+   for(var x = 0; x < arry.length; x++){    
+      for(var y = 0; y < arry[0].length; y++){ 
+          arry[x][y] =defalut_value;    
+      }    
+  }
+  return arry;
+}
 
 function initBoard()
 {
-   for(i = 0; i < 30; i++)
-      for(j = 0; j < 30; j++)
-         board[i][j] = 0;
+   board = get2DArray(30,30);
+   default_init_array(board, 0);
 
-   for(i = 0; i < 10; i++)
-      board[13][29-i] = 1;  // Mauer von oben nach unten (von Rand oben)
-   for(i = 0; i < 10; i++)  // von links nach rechts 
-      board[i][14] = 1;
-   for(i = 0; i < 7; i++)
-      board[18][i] = 1;  // Mauer von unten nach oben
-   for(i = 0; i < 10; i++)  // von links nach rechts 
-      board[29-i][12] = 1;
-  
-    drawBoard();  
+
+  for(i = 0; i < 10; i++)
+     board[13][29-i] = 1;  // Mauer von oben nach unten (von Rand oben)
+  for(i = 0; i < 10; i++)  // von links nach rechts 
+     board[i][14] = 1;
+  for(i = 0; i < 7; i++)
+     board[18][i] = 1;  // Mauer von unten nach oben
+  for(i = 0; i < 10; i++)  // von links nach rechts 
+     board[29-i][12] = 1;
+ 
+   drawBoard();  
 }
 
 function drawBoard()
 {
+   console.log(board);
+
     for(i = 0; i < 30; i++)
       for(j = 0; j < 30; j++)
-        drawOnePoint(i, j, board[i][j]);
-
-        
+        drawOnePoint(i, j, board[i][j]);      
 }
 
 function drawOnePoint(i, j, val)
@@ -45,18 +60,5 @@ function drawOnePoint(i, j, val)
     var canvas = document.getElementsByTagName('canvas')[0];
     ctx = canvas.getContext('2d');
 
-    ctx.beginPath();
-    ctx.moveTo(20*i, 600-20*j);
-    ctx.lineTo(20*i+19, 600-20*j);
-    ctx.lineTo(20*i+19, 600-20*j);
-    ctx.lineTo(0, 150);
-    ctx.stroke();
-
+    ctx.fillRect(i*20+10,j*20+10,1,1); 
 }
-
-
-
-//
-//ar test = new Linie(new Punkt(0,0), new Punkt(1,1));
-//
-//est.say_line();
